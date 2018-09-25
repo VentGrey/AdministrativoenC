2bc7db2fa93410a00bbad03cf24a28b5c1551219
VentGrey <omarpurataf@gmail.com>
2018-09-25 09:50:47 -0500

    fully formatted code to use 8 indent-spaces

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ struct cliente {
 
 //---Prototipos---
 void cargar(void), guardar(void), buscar(void), muestra(int i);
-void leercad(char * indic, char *s, int cont);
+void leercad(char *indic, char *s, int cont);
 void borrar(void), intro(void), mostrar(void);
 int busca(char *nombre), menu(void);
 void iniciar_lista(void);
@@ -71,33 +71,40 @@ int main() {
 void iniciar_lista(void)
 {
         /* No vimos esto en clase pero el usar la palabra clave "register"
-        * va a guardar una variable (En este caso de tipo entero) en el registro del procesador
+        * va a guardar una variable (En este caso de tipo entero) en el
+        * registro del procesador, los registros son menos tardados en acceder
+        * que la memoria, como el entero i que estoy declarando abajo es una
+        * variable que voy a usar frecuentemente es mejor guardarla dentro
+        * del registro aunque al final del día en compilador será quien
+        * decida poner la variable dentro del registro.
         */
         register int i;
 
-        for (i = 0; i < MAX; i++) *cliente[i].nombre = '\0'; //Un iterador simple para recorrer el arreglo de estructuras
+        for (i = 0; i < MAX; i++) *cliente[i].nombre = '\0';
 }
 
-int menu(void) //No estoy seguro si realmente necesito poner void 2 veces aquí
-{
-  char s[80]; //No mas de 80 caracteres, nadie quiere tipear nombres de opciones tan largos.
-  int c;
-  printf("1- Introducir un cliente\n");
-  printf("2- Borrar un cliente\n");
-  printf("3- Listar los clientes\n");
-  printf("4- Buscar en el archivo\n");
-  printf("5- Guardar el archivo\n");
-  printf("6- Cargar el archivo \n");
-  printf("7- Ordenar el archivo por nombre\n");
-  printf("8- Salir\n");
-  printf("9- Acerca De: ");
 
-  do {
-    printf("\nIntroduzca su opción: ");
-    fgets(s, sizeof(s), stdin);
-    c = atoi(s);
-  } while (c < 0 || c > 9);
-  return c;
+int menu(void)
+{
+        char s[80]; //No mas de 80 caracteres, nadie quiere tipear nombres de opciones tan largos.
+        int c;
+        printf("1- Introducir un cliente\n");
+        printf("2- Borrar un cliente\n");
+        printf("3- Listar los clientes\n");
+        printf("4- Buscar en el archivo\n");
+        printf("5- Guardar el archivo\n");
+        printf("6- Cargar el archivo \n");
+        printf("7- Ordenar el archivo por nombre\n");
+        printf("8- Salir\n");
+        printf("9- Acerca De: ");
+
+        do {
+                printf("\nIntroduzca su opción: ");
+                fgets(s, sizeof(s), stdin);
+                c = atoi(s);
+        } while (c < 0 || c > 9);
+
+        return c;
 }
 
 
@@ -111,34 +118,31 @@ int menu(void) //No estoy seguro si realmente necesito poner void 2 veces aquí
 
 void intro(void)
 {
-  int i;
+        int i;
 
-  for(;;) //Otro for infinito, lo mismo que un while(1), pero  otra vez, da mucha flojera escribir tanto.
-  {
-    i = busca_libre(); //Con esto buscaremos una estructura declarada que esté libre para almacenar datos.
+        for(;;) {
+                i = busca_libre();
 
-    if (i < 0)
-    {
+                if (i < 0) {
+                        printf("\nLa lista se encuentra llena :( \n");
+                        return;
+                }
 
+                leercad("\nIntroduzca un ID: ", cliente[i].ID, 8);
+                //Con esta función leeremos las cadenas de caracteres que se necesiten.
+                leercad("\nIntroduzca un nombre: ", cliente[i].nombre, 30);
+                //Condicional de espacio
+                if (! *cliente[i].nombre) break; //Podemos poner la sentencia en la misma línea que la condición.
+                leercad("\nIntroduzca un teléfono: ", cliente[i].telefono, 10); //Una funcion personalizada paraleer cadenas
 
-      printf("\nLa lista se encuentra llena :( \n");
-      return; //AFAIK esto si se puede hacer si la función no tiene un tipo y en sus parámetros ponemos solamente un void
-    }
+                printf("\nIntroduzca un día: ");
+                scanf("%x", &cliente[i].dia);
+                printf("\nIntroduzca un mes: ");
+                scanf("%x", &cliente[i].mes);
+                printf("\nIntroduzca un año: ");
+                scanf("%x", &cliente[i].anio);
 
-    leercad("\nIntroduzca un ID: ", cliente[i].ID, 8);
-    //Con esta función leeremos las cadenas de caracteres que se necesiten.
-    leercad("\nIntroduzca un nombre: ", cliente[i].nombre, 30);
-    //Condicional de espacio
-    if (! *cliente[i].nombre) break; //Podemos poner la sentencia en la misma línea que la condición.
-    leercad("\nIntroduzca un teléfono: ", cliente[i].telefono, 10); //Una funcion personalizada paraleer cadenas
-
-    printf("\nIntroduzca un día: ");
-    scanf("%x", &cliente[i].dia);
-    printf("\nIntroduzca un mes: ");
-    scanf("%x", &cliente[i].mes);
-    printf("\nIntroduzca un año: ");
-    scanf("%x", &cliente[i].anio);
-  }
+        }
 }
 
 
@@ -147,44 +151,45 @@ void intro(void)
  * Esta función va a retornar el índice (o el espacio) de una estructura (si es que hay una libre)
  * En caso de no existir, devolverá un -1 (No hay excepciones aquí) aunque podríamos usar
  *la librería errno.h pero solo sería para enviar un mensaje de error y no para ciclar el programa */
+
 int busca_libre(void)
 {
-  register int i; //De nuevo guardamos el entero "i" en un registro del procesador.
+        register int i; //De nuevo guardamos el entero "i" en un registro del procesador.
 
-  for(i = 0; i<MAX; i++) //Un for sin llaves hará que la siguiente instrucción pertenezca al cuerpo del ciclo.
-    if(!*cliente[i].nombre) return i;
-  return -1;
+        for(i = 0; i<MAX; i++) //Un for sin llaves hará que la siguiente instrucción pertenezca al cuerpo del ciclo.
+                if(!*cliente[i].nombre) return i;
+        return -1;
 }
 
 
 
 //Esta función leerá una cadena de caracteres, lo genial es que gracias a StackOverflow y a alguien de Freenode
 //previene que la cadena tenga caracteres de mas y la valida
+
 void leercad(char *indic, char *s, int cont)
 {
-  char cadena[255];
+        char cadena[255];
 
-  
-  do
-  {
-    printf("%s", indic);
-    fgets(cadena, sizeof(cadena), stdin);
-    if(strlen(cadena)>=cont) printf("\nLa cadena de texto que ingresó es demasiado larga.");
-  } while (strlen(cadena)>=cont);
-  strcpy(s, cadena);
+        do {
+                printf("%s", indic);
+                fgets(cadena, sizeof(cadena), stdin);
+                if(strlen(cadena)>=cont) printf("\nLa cadena de texto que ingresó es demasiado larga.");
+        } while (strlen(cadena)>=cont);
+
+        strcpy(s, cadena);
 }
 
 /* Esta función eliminará una dirección de la lista */
 
 void borrar(void)
 {
-    int i;
-    char cadena[255];
+        int i;
+        char cadena[255];
 
-    leercad("Introduzca el nombre del usuario que desea eliminar: ", cadena, 30);
-    i = busca(cadena);
-    if ( i >= 0 ) *cliente[i].nombre = '\0';
-    else printf("No se encontró a esa persona\n");
+        leercad("Introduzca el nombre del usuario que desea eliminar: ", cadena, 30);
+        i = busca(cadena);
+        if ( i >= 0 ) *cliente[i].nombre = '\0';
+        else printf("No se encontró a esa persona\n");
 }
 
 /* Ésta función buscará un nombre y devolverá su lugar en el índice
@@ -192,66 +197,63 @@ void borrar(void)
 
 int busca(char *nombre)
 {
-    int i;
+        int i;
 
-    for(i=0; i < MAX; i++)
+        for(i=0; i < MAX; i++)
         if (!strcmp(nombre, cliente[i].nombre)) break;
-    if (i == MAX) return -1; //Solo si ya se recorrió todo el arreglo y no se encontró el nombre.
-    else return i;
+        if (i == MAX) return -1; //Solo si ya se recorrió todo el arreglo y no se encontró el nombre.
+        else return i;
 }
 
 // Con la siguiente función mostraremos la lista de correos
 
 void mostrar(void)
 {
-    int i;
+        int i;
 
-    for(i=0; i<MAX; i++)
-    {
-        if(*cliente[i].nombre)
-        {
-            muestra(i);
-            printf("\n\n");
+        for(i=0; i<MAX; i++) {
+                if(*cliente[i].nombre) {
+                        muestra(i);
+                        printf("\n\n");
+                }
         }
-    }
-    printf("\n\n");
+        printf("\n\n");
 }
 // Lo siguiente imprimirá los nombres y los teléfonos
 
 void muestra(int i)
 {
-    printf("%s\n", cliente[i].ID);
-    printf("%s\n", cliente[i].nombre);
-    printf("%s %x %x %x \n", cliente[i].telefono, cliente[i].dia, cliente[i].mes, cliente[i].anio);
+        printf("%s\n", cliente[i].ID);
+        printf("%s\n", cliente[i].nombre);
+        printf("%s %x %x %x \n", cliente[i].telefono, cliente[i].dia, cliente[i].mes, cliente[i].anio);
 }
 
 /* Buscar dentro de la lista */
 void buscar(void)
 {
-    int i;
-    char nombre[30];
+        int i;
+        char nombre[30];
 
-    leercad("Introduzca sus parámetros de búsqueda: ", nombre, 30);
-    if((i=busca(nombre))< 0) printf("No se encontró a nadie con ese nombre\n");
-    else muestra(i);
+        leercad("Introduzca sus parámetros de búsqueda: ", nombre, 30);
+        if((i=busca(nombre))< 0) printf("No se encontró a nadie con ese nombre\n");
+        else muestra(i);
 }
 
 /* Guardado y cargado de la lista */
 
 void guardar(void)
 {
-    FILE *fp;
+        FILE *fp;
 
-    if((fp = fopen("clientes.gdnz", "wb")) == NULL)
-    {
-        printf("No se pudo abrir el archivo :(\n");
-        return;
-    }
+        if((fp = fopen("clientes.gdnz", "wb")) == NULL) {
+                printf("No se pudo abrir el archivo :(\n");
+                return;
+        }
 
-    printf("\n**********|Guardando el archivo|***********\n");
-    fwrite(cliente, sizeof cliente, 1, fp);
-    if(ferror(fp))
-        printf("Ocurrió un error al escribir en el archivo :(\n");
+        printf("\n**********|Guardando el archivo|***********\n");
+        fwrite(cliente, sizeof cliente, 1, fp);
+        if(ferror(fp))
+                printf("Ocurrió un error al escribir en el archivo :(\n");
         fclose(fp);
 }
 
@@ -259,16 +261,16 @@ void guardar(void)
 
 void cargar(void)
 {
- FILE *fp;
+        FILE *fp;
 
- if((fp = fopen("clientes.gdnz", "rb")) == NULL)
- {
-     printf("No se pudo cargar el archivo :( \n");
-     return;
- }
+        if((fp = fopen("clientes.gdnz", "rb")) == NULL) {
+                printf("No se pudo cargar el archivo :( \n");
+                return;
+        }
 
-printf("\n**********|Cargando el archivo|**********\n");
-fread(cliente, sizeof cliente, 1, fp);
-if(ferror(fp))
-  printf("Ocurrió un error al cargar el archivo :(\n");
+        printf("\n**********|Cargando el archivo|**********\n");
+        fread(cliente, sizeof cliente, 1, fp);
+
+        if(ferror(fp))
+                printf("Ocurrió un error al cargar el archivo :(\n");
 }
